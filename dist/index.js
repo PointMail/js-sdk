d1a29ef0737f644bc631cf85f25ca8b4964bee9d
Alexander Iansiti <aiansiti@outlook.com>
2018-06-05 18:31:02 -0400

    switch to sockets. not working yet cause CORS

--- dist/index.js
+++ dist/index.js
@@ -1,92 +1,57 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('axios')) :
-  typeof define === 'function' && define.amd ? define(['axios'], factory) :
-  (global['point-api'] = factory(global.axios));
-}(this, (function (axios) { 'use strict';
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('socket.io-client')) :
+  typeof define === 'function' && define.amd ? define(['socket.io-client'], factory) :
+  (global['point-api'] = factory(global.io));
+}(this, (function (io) { 'use strict';
 
-  axios = axios && axios.hasOwnProperty('default') ? axios['default'] : axios;
-
-  //     
-
-  function fuzzysearch(needle        , haystack        )          {
-    // Fuzzy search as done by Matt.
-    const split_in = needle.split(/(\s+)/, 1);
-    if (split_in.length > 1) {
-      if (!haystack.split(/(\s+)/, 5).includes(split_in[0])) {
-        return false;
-      }
-    } else {
-      if (!haystack.startsWith(needle)) {
-        return false;
-      }
-    }
-    var hlen = haystack.length;
-    var nlen = needle.length;
-    if (nlen > hlen) {
-      return false;
-    }
-    if (nlen === hlen) {
-      return needle === haystack;
-    }
-    outer: for (var i = 0, j = 0; i < nlen; i++) {
-      var nch = needle.charCodeAt(i);
-      while (j < hlen) {
-        if (haystack.charCodeAt(j++) === nch) {
-          continue outer;
-        }
-      }
-      return false;
-    }
-    return true;
-  }
+  io = io && io.hasOwnProperty('default') ? io['default'] : io;
 
   //     
 
-  const ROOT_URI = "https://alpha-autocomplete.easyemail.ai";
-
-                                                                                 
+                                
+                       
+                       
+                
+    
 
   class PointApi {
                          
-                     
+                   
                                        
+                   
 
-    constructor(emailAddress        , authCode        ) {
+    constructor(emailAddress        , apiKey        ) {
       this.emailAddress = emailAddress;
-      this.authCode = authCode;
+      this.apiKey = apiKey;
       this.suggestions = [];
+      this.socket = io(
+        "http://dev-api-autocomplete.us-west-2.elasticbeanstalk.com",
+        {
+          query: {
+            emailAddress: "przxmek@gmail.com"
+          },
+          transportOptions: {
+            polling: {
+              extraHeaders: {
+                Authorization: "Basic " + this.apiKey
+              }
+            }
+          }
+        }
+      );
     }
 
-    async getSuggestions() {
-      try {
-        const result = await axios.get(ROOT_URI + "/suggestions", {
-          params: { emailAddress: this.emailAddress, authCode: this.authCode }
+    searchSuggestions(query        )                                  {
+      return new Promise(resolve => {
+        if (!query || !query.trim()) {
+          resolve(null);
+        }
+        this.socket.emit("suggestions", { seedText: query }, suggestions => {
+          if (!suggestions || !suggestions.length) {
+            resolve(null);
+          }
+          resolve(suggestions);
         });
-        this.suggestions = result.data.suggestions.slice();
-        return this.suggestions;
-      } catch (e) {
-        throw new Error("Unable to get suggestions");
-      }
-    }
-
-    async searchSuggestions(query        )                                 {
-      if (!this.suggestions.length) {
-        throw new Error("Suggestions not loaded");
-      }
-      if (!query || !query.trim()) {
-        return [];
-      }
-      const punctuationReg = /[,\/;:\-_()']/g;
-      query = query
-        .toLowerCase()
-        .trim()
-        .replace(punctuationReg, "");
-      return this.suggestions.filter(meta => {
-        const sentence = meta.suggestion
-          .toLowerCase()
-          .replace(punctuationReg, "");
-        if (sentence === query) return false;
-        return fuzzysearch(query, sentence); //fuzzy search on the entire database of answers. Search isn't case-specific.
       });
     }
   }
