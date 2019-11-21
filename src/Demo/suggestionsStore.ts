1f0db6598cea1fadafb9d5d628b46af3d3a4cb0f
Lambert Chu <lambert.chu@gmail.com>
2019-11-21 10:56:29 -0500

    Delete old demo snippets

--- src/Demo/suggestionsStore.ts
+++ src/Demo/suggestionsStore.ts
@@ -2,11 +2,7 @@ import { Snippet } from "../ApiModules/autocompleteSession";
 
 const INITIAL_SNIPPETS: Snippet[] = [
   { id_: '1', content: "Hi there, it's great to hear from you!", name: 'hello', labels: [] },
-  { id_: '2', content: "Thank you for reaching out. We're currently all set, but I will make sure to ping you should our needs change.", name: 'thanks', labels: [] },
-  { id_: '3', content: 'Nice to meet you!', name: 'intro1', labels: [] },
-  { id_: '4', content: 'Nice to hear from you.', name: 'intro2', labels: [] },
-  { id_: '5', content: 'How have you been?', name: 'intro3', labels: [] },
-  { id_: '6', content: "Point uses AI to predict what you want to say and writes it in your own words. With our suggestions, you will communicate quickly, intelligently, and effortlessly.", name: 'about', labels: ['tutorial'] }
+  { id_: '2', content: "Thank you for reaching out. We're currently all set, but I will make sure to ping you should our needs change.", name: 'thanks', labels: [] }
 ];
 
 export default class SuggestionsStore {
